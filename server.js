e5f8c93f3187181ed1cb47cb2445b9623e126e77
edasde72 <krystofpospisil7@seznam.cz>
2026-01-28 20:00:44 +0100

    server.js

--- server.js
+++ server.js
@@ -191,48 +191,58 @@ io.on("connection", socket => {
     });
   }
 
-  function broadcastResult(room, winner) {
-    const isFinal = room.gameState.round >= 5;
-    
-    io.to(room.host).emit("round-result", {
-      winner,
-      isHost: true,
-      countryName: room.gameState.currentCountry[0],
-      hostScore: room.gameState.hostScore,
-      guestScore: room.gameState.guestScore,
-      isFinal
-    });
-    
-    io.to(room.guest).emit("round-result", {
-      winner,
-      isHost: false,
-      countryName: room.gameState.currentCountry[0],
-      hostScore: room.gameState.hostScore,
-      guestScore: room.gameState.guestScore,
-      isFinal
-    });
-    
-    if (!isFinal) {
-      room.gameState.round++;
-      setTimeout(() => startNewRound(currentRoom), 5000);
+  function broadcastResult(room, roundWinner) {
+  const isFinal = room.gameState.round >= 5;
+  
+  // Výsledek kola (kdo uhodl tuto zemi)
+  io.to(room.host).emit("round-result", {
+    winner: roundWinner,
+    isHost: true,
+    countryName: room.gameState.currentCountry[0],
+    hostScore: room.gameState.hostScore,
+    guestScore: room.gameState.guestScore,
+    isFinal
+  });
+  
+  io.to(room.guest).emit("round-result", {
+    winner: roundWinner,
+    isHost: false,
+    countryName: room.gameState.currentCountry[0],
+    hostScore: room.gameState.hostScore,
+    guestScore: room.gameState.guestScore,
+    isFinal
+  });
+  
+  if (!isFinal) {
+    room.gameState.round++;
+    setTimeout(() => startNewRound(currentRoom), 5000);
+  } else {
+    // KONEC HRY - určení celkového vítěze podle skóre!
+    let gameWinner;
+    if (room.gameState.hostScore > room.gameState.guestScore) {
+      gameWinner = "host";
+    } else if (room.gameState.hostScore < room.gameState.guestScore) {
+      gameWinner = "guest";
     } else {
-      setTimeout(() => {
-        io.to(room.host).emit("game-over", {
-          winner,
-          isHost: true,
-          hostScore: room.gameState.hostScore,
-          guestScore: room.gameState.guestScore
-        });
-        io.to(room.guest).emit("game-over", {
-          winner,
-          isHost: false,
-          hostScore: room.gameState.hostScore,
-          guestScore: room.gameState.guestScore
-        });
-      }, 5000);
+      gameWinner = "draw";
     }
+    
+    setTimeout(() => {
+      io.to(room.host).emit("game-over", {
+        winner: gameWinner,  // ← Tady je oprava - gameWinner místo roundWinner
+        isHost: true,
+        hostScore: room.gameState.hostScore,
+        guestScore: room.gameState.guestScore
+      });
+      io.to(room.guest).emit("game-over", {
+        winner: gameWinner,  // ← Tady taky gameWinner
+        isHost: false,
+        hostScore: room.gameState.hostScore,
+        guestScore: room.gameState.guestScore
+      });
+    }, 5000);
   }
-});
+}
 
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
